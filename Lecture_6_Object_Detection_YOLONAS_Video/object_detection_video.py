3dd32de3d4298bd9c07fd1486b8433b17fc55133
avinash <avinashmaurya539239@gmail.com>
2023-12-22 00:32:30 +0530

    update

--- Lecture_6_Object_Detection_YOLONAS_Video/object_detection_video.py
+++ Lecture_6_Object_Detection_YOLONAS_Video/object_detection_video.py
@@ -1,41 +1,46 @@
+
 import cv2
+# import super_gradients
 import torch
 from super_gradients.training import models
 import numpy as np
 import math
 
-cap = cv2.VideoCapture("..Video/traffic.mp4")
+# Replace with the correct path to your video file
+cap = cv2.VideoCapture("../Video/airport.mp4")
 
-device = torch.device("cuda:0") if torch.cuda.is_available() else torch.device("cpu")
+device = torch.device(
+    "cuda:0") if torch.cuda.is_available() else torch.device("cpu")
 
+# Replace 'models.get' with the correct method or function to load the YOLO model
 model = models.get('yolo_nas_s', pretrained_weights="coco").to(device)
 
-count =0
+count = 0
 
 while True:
-    ret,frame = cap.read()
+    ret, frame = cap.read()
     count += 1
     if ret:
         result = list(model.predict(frame, conf=0.35))[0]
         bbox_xyxys = result.prediction.bboxes_xyxy.tolist()
         confidences = result.prediction.confidence
-        labels = result.prediction.label.tolist()
+        labels = result.prediction.labels.tolist()
         for (bbox_xyxys, confidence, cls) in zip(bbox_xyxys, confidences, labels):
             bbox = np.array(bbox_xyxys)
             x1, y1, x2, y2 = bbox[0], bbox[1], bbox[2], bbox[3]
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
             classname = int(cls)
-            conf = math.ceil((confidence*100))/100
+            conf = math.ceil((confidence * 100)) / 100
             label = f'{classname}{conf}'
             print("Frame N", count, "", x1, y1, x2, y2)
-            t_size = cv2.getTextSize(label, 0, fontScale=1,thickness=2)
-            c2=x1 + t_size[0], y1 - t_size[1] -3
+            t_size = cv2.getTextSize(label, 0, fontScale=1, thickness=2)[0]
+            c2 = x1 + t_size[0], y1 - t_size[1] - 3
             cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 255), 3)
-
+#
         cv2.imshow("Frame", frame)
         if cv2.waitKey(1) & 0xFF == ord('1'):
             break
-        else:
-            break
+    else:
+        break
 
 cv2.destroyAllWindows()
